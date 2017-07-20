572b7f97375cb86e32f4714ed7dc262621fed5e2
osmanmusa <30294709+osmanmusa@users.noreply.github.com>
2017-07-20 08:47:40 +0200

    Changes for func_SE_MC.m

--- func_SE_MC.m
+++ func_SE_MC.m
@@ -1,20 +1,32 @@
-function [ se_tau2, se_mse ] = func_SE_MC(tau2, delta, rho, sigma2)
+function [ se_tau2, se_mse ] = func_SE_MC(tau2, delta, rho, sigmaw2)
 % SigmaX is the variance of the nonzero components in the signal.
 % SigmaW is the variance of the effective noise.
 % rho is the probability of a nonzero component, i.e., sparsity ratio (K/N).
   
-MC      = 1e6;                    % No. of MC experiments.
+if(sigmaw2 == 0)
+    deniser_parameter = sqrt(tau2);
 
-ind = randperm(MC);
-x = zeros(MC,1);
-x(ind(1:round(MC*rho,0))) = randn(round(MC*rho,0),1);
-z = randn(MC,1);
-
-if(sigma2 == 0)
-    se_mse = 1/MC*sum((wthresh(x + sqrt(tau2)*z,'s',sqrt(tau2)) - x).^2);
-    se_tau2 = sigma2 + 1/delta * se_mse;
 else
     lambda = 0.5;
-    se_mse = 1/MC*sum((wthresh(x + sqrt(tau2)*z,'s',sqrt(lambda+lambda*tau2)) - x).^2);
-    se_tau2 = sigma2 + 1/delta * se_mse;
+    deniser_parameter = sqrt(lambda+lambda*tau2);
+end
+
+
+% Gauss part
+f1 = @(x,z) (rho.*reshape(normpdf(x(:),0,1),size(x)) ) ...
+            .* reshape(normpdf(z(:),0,deniser_parameter),size(z)) ...
+            .* (wthresh(x + z,'s',deniser_parameter) - x).^2;
+
+% Dirac part        
+f2 = @(z) (1-rho) ...
+            .* reshape(normpdf(z(:),0,deniser_parameter),size(z)) ...
+            .* (wthresh( z,'s',deniser_parameter)).^2;        
+
+
+lim = inf;
+
+se_mse = (integral2(f1,-lim,lim,-lim,lim) + integral(f2,-lim,lim));
+se_tau2 = sigmaw2 + 1/delta * se_mse;   
+
+
 end
