f82fc3779a40bbde480dfe95f67b306477a8a519
osmanmusa <30294709+osmanmusa@users.noreply.github.com>
2017-08-01 15:20:52 +0200

    deleted unnecessary files

--- unthresholdedestimateanalysis.m
+++
@@ -1,8 +0,0 @@
-
-load('variables')
-histogram(reshape(AMP_unthresholded_residuals,MC*K,1), 100,'Normalization','pdf')
-y = fitdist(reshape(AMP_unthresholded_residuals,MC*K,1), 'Normal');
-hold on
-x = -1:0.01:1;
-f = normpdf(x, y.mu, y.sigma);
-plot(x,f,'r')
